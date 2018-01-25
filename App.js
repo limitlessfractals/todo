7af21625ff4dd9dd7a31beee75db6c985258383b
limitlessfractals <fanwuq@gmail.com>
2018-01-25 18:22:44 -0500

    state

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import {View,Text, TextInput, Button} from 'react-native';
 
 class App extends React.Component{
+  state = {
+    text: "This is our message on screen",
+    todo: " "
+  }
+  // es6 function, setState, not =
+  addTodo = () =>{
+    this.setState({todo: this.state.text});
+  }
   render(){
     return(
       <View style={styles.viewStyle}>
         <Text>Hello World!</Text>
         <TextInput
           style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
         />
         <Button
           title="Add Todo"
+          onPress={this.addTodo}
         />
+        <Text>{this.state.todo}</Text>
       </View>
     )
   }
