201a28a3cb83acb18a6444107a0112315c0ebac3
limitlessfractals <fanwuq@gmail.com>
2018-01-25 22:35:01 -0500

    styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,Text, TextInput, Button} from 'react-native';
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
 
 class App extends React.Component{
   state = {
@@ -22,43 +22,64 @@ class App extends React.Component{
   renderTodos = () =>{
     return this.state.todo.map(t=>{
       return (
-        <Text 
-          key={t}
-          onPress={()=>{this.deleteTodo(t)}}
-        >{t}</Text>
+        <TouchableOpacity key={t}>
+          <Text
+            style={styles.todo}
+            onPress={()=>{this.deleteTodo(t)}}
+          >{t}</Text>
+        </TouchableOpacity>
       )
     })
   }
   render(){
     return(
-      <View style={styles.viewStyle}>
-        <Text>Todo List</Text>
-        <TextInput
-          style={styles.inputStyle}
-          onChangeText={(text)=>this.setState({text})}
-          value={this.state.text}
-        />
-        <Button
-          title="Add Todo"
-          onPress={this.addTodo}
-        />
-        {this.renderTodos()}
+      <View style={styles.wholeStyle}>
+        <View style={styles.viewStyle}>
+          <Text style={styles.header}>Todo List</Text>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={(text)=>this.setState({text})}
+            value={this.state.text}
+          />
+          <Button
+            title="Add Todo"
+            color='#fbe4e4'
+            onPress={this.addTodo}
+          />
+          <View style={{marginTop: 50}}/>
+          {this.renderTodos()}
+        </View>
       </View>
     )
   }
 }
 
 const styles = {
+  wholeStyle: {
+    backgroundColor: '#1e2e4d',
+    flex: 1
+  },
   viewStyle: {
-    flex: 1,
+    marginTop: 30,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 10
   },
   inputStyle:{
     height: 40,
     width: 300,
-    borderColor: "blue",
-    borderWidth: 1
+    borderColor: "#fbe4e4",
+    borderWidth: 1,
+    color: '#fbe4e4'
+  },
+  header:{
+    fontSize: 30,
+    color: '#fbe4e4',
+    fontWeight: 'bold'
+  },
+  todo:{
+    fontSize: 24,
+    color: '#fbe4e4'
   }
 }
 
